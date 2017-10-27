303533bb9848948510be0b6f61749b1f785474a1
harrypotter0 <9654263057akashkandpal@gmail.com>
2017-10-28 02:40:16 +0530

    Blocks running :)

--- snakecoin.py
+++ snakecoin.py
@@ -9,7 +9,7 @@ class Block:
     self.data = data
     self.previous_hash = previous_hash
     self.hash = self.hash_block()
-
+  
   def hash_block(self):
     sha = hasher.sha256()
     sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash))
@@ -44,4 +44,4 @@ for i in range(0, num_of_blocks_to_add):
   previous_block = block_to_add
   # Tell everyone about it!
   print "Block #{} has been added to the blockchain!".format(block_to_add.index)
-  print "Hash: {}\n".format(block_to_add.hash)
+  print "Hash: {}\n".format(block_to_add.hash) 
